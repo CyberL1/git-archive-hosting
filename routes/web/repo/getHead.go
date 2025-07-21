701ff547dc11e4e3e1b6b56f7bcd1d1a3288785e
CyberL1 <cyber.hf18@gmail.com>
2025-07-21 14:16:40 +0200

    fix(web): remove not needed code

--- routes/web/repo/getHead.go
+++ routes/web/repo/getHead.go
@@ -6,7 +6,6 @@ import (
 	"garg/utils"
 	"net/http"
 	"path/filepath"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 	"gopkg.in/src-d/go-git.v4"
@@ -15,18 +14,6 @@ import (
 func GetHead(c *gin.Context) {
 	repoOwner := c.Param("owner")
 	repoName := c.Param("repo")
-
-	// Only allow smart HTTP requests:
-	if !strings.Contains(c.Request.UserAgent(), "git/") {
-		c.String(http.StatusForbidden, "Access denied: Not a git client")
-		return
-	}
-
-	// Accept only smart HTTP query param or content type
-	if c.Query("service") != "git-upload-pack" && c.ContentType() != "application/x-git-upload-pack-request" {
-		c.String(http.StatusForbidden, "Access denied: Not a smart HTTP request")
-		return
-	}
 
 	repo, err := git.PlainOpen(filepath.Join(constants.RepositoriesDir, repoOwner, utils.AppendDotGitExt(repoName)))
 	if err != nil {
